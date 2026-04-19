dffa61e932c5acb6cd9c7a9dbed7a9953976571f
AmanSaxena08 <AmanSaxena08@users.noreply.github.com>
2026-04-19 14:46:39 +0530

    Fix superuser auto creation in build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -4,16 +4,16 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input --clear
 python manage.py migrate
-python manage.py shell -c "
+python manage.py shell << 'EOF'
 from django.contrib.auth import get_user_model
-User = get_user_model()
 import os
+User = get_user_model()
 username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
 email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
 password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'Admin@1234')
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
-    print('Superuser created')
+    print('Superuser created successfully')
 else:
     print('Superuser already exists')
-"
+EOF
